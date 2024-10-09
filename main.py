126e7fc75eedfc074c46baebd49adefa8bf2c52c
chaosi-zju <chaosi@zju.edu.cn>
2024-10-10 02:38:54 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import akshare as ak
+
+stock_intraday_em_df = ak.stock_intraday_em(symbol="000001")
+print(stock_intraday_em_df)
+
+# import akshare as ak
+#
+#
+#
+# stock_intraday_sina_df = ak.stock_intraday_sina(symbol="sh563300", date="20241008")
+# print(stock_intraday_sina_df)
