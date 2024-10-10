c06a9bd97dc9a8fa19c7f0ee720243ea49171381
chaosi-zju <chaosi@zju.edu.cn>
2024-10-11 03:25:56 +0800

    sse client

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.22.1
 
 require (
 	github.com/go-resty/resty/v2 v2.15.3 // indirect
+	github.com/r3labs/sse/v2 v2.10.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
+	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,29 +2,52 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
-	"github.com/go-resty/resty/v2"
+	"github.com/r3labs/sse/v2"
 )
-
-var client = resty.New()
 
 func main() {
 	symbol := "0.000001"
+	baseUrl := "https://70.push2.eastmoney.com/api/qt/stock/details/sse"
+	params := map[string]string{
+		"fields1": "f1,f2,f3,f4",
+		"fields2": "f51,f52,f53,f54,f55",
+		"mpi":     "2000",
+		"ut":      "bd1d9ddb04089700cf9c27f6f7426281",
+		"fltt":    "2",
+		"pos":     "-0",
+		"secid":   symbol,
+		"wbp2u":   "|0|0|0|web",
+	}
 
-	resp, err := client.R().
-		SetQueryParams(map[string]string{
-			"fields1": "f1,f2,f3,f4",
-			"fields2": "f51,f52,f53,f54,f55",
-			"mpi":     "2000",
-			"ut":      "bd1d9ddb04089700cf9c27f6f7426281",
-			"fltt":    "2",
-			"pos":     "-0",
-			"secid":   symbol,
-			"wbp2u":   "|0|0|0|web",
-		}).Get("https://70.push2.eastmoney.com/api/qt/stock/details/sse")
+	//events := make(chan *sse.Event, 100)
+
+	url := urlWithParams(baseUrl, params)
+	fmt.Println(url)
+	client := sse.NewClient(url)
+
+	err := client.SubscribeRaw(func(msg *sse.Event) {
+		// Got some data!
+		fmt.Printf("%s", msg.Data)
+	})
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	fmt.Printf("%s\n", resp)
+}
+
+func urlWithParams(baseUrl string, params map[string]string) string {
+	base, err := url.Parse(baseUrl)
+	if err != nil {
+		fmt.Println(err)
+		return baseUrl
+	}
+
+	values := url.Values{}
+	for k, v := range params {
+		values.Add(k, v)
+	}
+	base.RawQuery = values.Encode()
+
+	return base.String()
 }
